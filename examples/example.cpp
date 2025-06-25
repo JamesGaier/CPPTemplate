a97b3cc31aeffb90ae16872d0ae3429f7697e998
James Gaier <james.gaier003@gmail.com>
2025-06-24 20:41:20 -0700

    Add examples of template style

---
+++ examples/example.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace bar
+{
+
+class Foo
+{
+};
+
+}  // namespace bar
+
+enum class MyEnum
+{
+  A,
+  B,
+  C
+};
+
+int main()
+{
+  MyEnum a;
+}
---
+++ examples/example1.cpp
@@ -0,0 +1,23 @@
+#include <atomic>
+#include <iostream>
+
+namespace bar
+{
+
+class Foo
+{
+};
+
+}  // namespace bar
+
+enum class MyEnum
+{
+  A,
+  B,
+  C
+};
+
+int main()
+{
+  MyEnum a;
+}
